8e5f0cc0b2e711d51d61b011dc793a0b1aa337b6
carlosvasquez3 <160137573+carlosvasquez3@users.noreply.github.com>
2026-03-11 19:10:01 +0000

    Se actualiza el json

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ function buscar(){
 let texto = document.getElementById("busqueda").value.toUpperCase()
 
 let resultados = medicamentos.filter(m =>
-    m.producto.includes(texto) ||
-    m.cum.includes(texto)
+    (m.producto && m.producto.toUpperCase().includes(texto)) ||
+    (m.cum && m.cum.includes(texto))
 )
 
 let html = ""
 
-resultados.forEach(r=>{
-html += `<p>${r.cum} - ${r.producto}</p>`
+resultados.forEach(r => {
+
+let codigo = r.codigo ? r.codigo : "NO EN NOPOS"
+
+html += `
+<div>
+<b>${r.producto}</b><br>
+CUM: ${r.cum}<br>
+FORMA FARMACEUTICA: ${r.formafarmaceutica}<br>
+CODIGO NOPOS: ${codigo}
+</div>
+<hr>
+`
+
 })
 
 document.getElementById("resultados").innerHTML = html
